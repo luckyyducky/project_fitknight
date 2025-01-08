91f1b2f7738e71a7e71d43a001710c4ecea69ac9
luckyyducky <aishpundirme@gmail.com>
2025-01-08 19:52:29 +0530

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import User 
+from .models import User
 from .models import BuddyProfile
 from .models import GroupProfile
 
 admin.site.register(User)
 admin.site.register(BuddyProfile)
-admin.site.register(GroupProfile)
+
+@admin.register(GroupProfile)
+class GroupProfileAdmin(admin.ModelAdmin):
+    fields = ('name', 'creator', 'activity_types', 'schedules', 'locations')  # Explicitly listing fields
+    list_display = ('name', 'creator')
+
+    def get_activity_types(self, obj):
+        return ", ".join(obj.activity_types)
+    get_activity_types.short_description = 'Activity Types'
+
+    def get_schedules(self, obj):
+        return ", ".join(obj.schedules)
+    get_schedules.short_description = 'Schedules'
+
+    def get_locations(self, obj):
+        return ", ".join(obj.locations)
+    get_locations.short_description = 'Locations'
