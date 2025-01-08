cf344f7f678eddad69451f2bf48a977c6bc31392
luckyyducky <aishpundirme@gmail.com>
2025-01-08 19:50:42 +0530

    models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField 
+import json
 
 
 class User(AbstractUser):
@@ -26,19 +28,62 @@ class User(AbstractUser):
     )
 
 class BuddyProfile(models.Model):
+    FITNESS_GOALS_CHOICES = [
+        ('Weight Loss', 'Weight Loss'),
+        ('Muscle Gain', 'Muscle Gain'),
+        ('General Fitness', 'General Fitness'),
+        ('Cardio', 'Cardio'),
+        ('Flexibility', 'Flexibility'),
+    ]
+    WORKOUT_PREFERENCES_CHOICES = [
+        ('gym', 'Gym'),
+        ('yoga', 'Yoga'),
+        ('running', 'Running'),
+        ('cycling', 'Cycling'),
+        ('swimming', 'Swimming'),
+    ]
+    AVAILABILITY_CHOICES = [    
+        ('morning', 'Morning'),
+        ('afternoon', 'Afternoon'),
+        ('evening', 'Evening'),
+        ('weekends', 'Weekends'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    fitness_goals = models.CharField(max_length=255, blank=True)
-    workout_preferences = models.CharField(max_length=255, blank=True)
-    availability = models.CharField(max_length=255, blank=True)
+    fitness_goals = MultiSelectField(choices=FITNESS_GOALS_CHOICES,max_choices=5, blank=True)
+    workout_preferences = MultiSelectField(choices=WORKOUT_PREFERENCES_CHOICES, max_choices=5, blank=True)
+    availability = MultiSelectField(choices=AVAILABILITY_CHOICES, max_choices=4, blank=True)
 
     def __str__(self):
         return f"Buddy Profile of: {self.user.username}"
 
 class GroupProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    activity_type = models.CharField(max_length=255, blank=True)
-    location = models.CharField(max_length=255, blank=True)
-    schedule = models.CharField(max_length=255, blank=True)
+    ACTIVITY_TYPES = [
+        ('Cardio', 'Cardio'),
+        ('Strength Training', 'Strength Training'),
+        ('Meditation', 'Meditation'),
+        ('Team Sports', 'Team Sports'),
+    ]
+    
+    SCHEDULES = [
+        ('Daily', 'Daily'),
+        ('Weekly', 'Weekly'),
+        ('Biweekly', 'Biweekly'),
+    ]
 
+    LOCATIONS = [
+        ('Gym A', 'Gym A'),
+        ('Gym B', 'Gym B'),
+        ('Park', 'Park'),
+        ('Community Center', 'Community Center'),
+    ]
+
+    creator = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    activity_types = MultiSelectField(choices=ACTIVITY_TYPES, max_choices=4, blank=True)
+    schedules = MultiSelectField(choices=SCHEDULES , max_choices=3, blank=True)
+    locations = MultiSelectField(choices=LOCATIONS, max_choices=4, blank=True)
+
+    
     def __str__(self):
-         return f"Group Profile of: {self.user.username}"
+        return f"Group Organizer Profile of: {self.creator.username}"
