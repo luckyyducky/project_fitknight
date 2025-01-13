09389dee796d3c137240fbff50c4976108cdba9f
luckyyducky <aishpundirme@gmail.com>
2025-01-13 12:23:14 +0530

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,55 +1,68 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
-from .models import BuddyProfile, GroupProfile
-from django.core.exceptions import ValidationError
-import json
 
 class CustomUserCreationForm(UserCreationForm):
     CHOICES = (
         ('buddy_finder', 'Buddy Finder'),
         ('group_organizer', 'Group Organizer'),
     )
+    
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    phone_number = forms.CharField(
+        max_length=15,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     user_type = forms.ChoiceField(
-        choices=CHOICES, 
-        widget=forms.RadioSelect,
+        choices=CHOICES,
+        widget=forms.RadioSelect(attrs={'class': 'form-check-input'}),
         label='User type'
     )
-    profile_picture = forms.ImageField(required=False)
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'password1', 'password2', 'profile_picture', 'user_type']
+        fields = ['username', 'email', 'phone_number', 'password1', 'password2', 'user_type']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Remove all help_text for username
-        self.fields['username'].help_text = ''
-        self.fields['password1'].help_text = None
-        self.fields['password2'].help_text = 'Enter the same password as before, for verification.'
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if User.objects.filter(phone_number=phone_number).exists():
+            raise forms.ValidationError("This phone number is already registered.")
+        return phone_number
     
+    def clean_user_type(self):
+        user_type = self.cleaned_data.get('user_type')
+        if user_type == 'buddy_finder':
+            self.instance.is_buddy_finder = True
+            self.instance.is_group_organizer = False
+        elif user_type == 'group_organizer':
+            self.instance.is_group_organizer = True
+            self.instance.is_buddy_finder = False
+        return user_type
+
 class CustomAuthenticationForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password'].widget.attrs.update({'class': 'form-control'})
-
-class BuddyProfileForm(forms.ModelForm):
-    class Meta:
-        model = BuddyProfile
-        fields = ['fitness_goals', 'workout_preferences', 'availability']
-        widgets = {
-            'fitness_goals': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'workout_preferences': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'availability': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-        }
-
-class GroupProfileForm(forms.ModelForm):
-    class Meta:
-        model = GroupProfile
-        fields = ['name', 'activity_types', 'schedules', 'locations']
-        widgets = {
-            'activity_types': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'schedules': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'locations': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-        }
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
